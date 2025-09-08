879099f27ff5f19ce422595a57596491ec709224
rao119349 <sauravyadav1193@gmail.com>
2025-09-08 21:05:23 +0530

    quiz start screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+// import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+
+  // 'loading','error','ready','active','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, questions: action.payload, status: "error" };
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 export default function App() {
+  const [{questions,status}, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch(
+      "https://thereactquizapp-qa43--8000--96435430.local-credentialless.webcontainer.io/questions"
+    )
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({type:'dataFailed'}));
+  }, []);
+
   return (
     <div className="app">
       {/* <DateCounter /> */}
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question ?</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions = {numQuestions} />}
       </Main>
     </div>
   );
