d49d082c1aa947dcd0a644956256ddf96b40dd19
rao119349 <sauravyadav1193@gmail.com>
2025-10-13 01:10:09 +0530

    changes package

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,7 +59,7 @@ export default function App() {
 
   useEffect(function () {
     fetch(
-      "http://localhost:9000/questions"
+      "http://localhost:8000/questions"
     )
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
